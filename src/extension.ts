fb513ca67c47b099225fac0a4c07584d2cfc3711
Spyros Georgiadis <s.georgiadis@pmmretail.com>
2023-04-28 14:16:08 +0300

    major refactoring added open drawer and populate drawer functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,71 +1,59 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as path from 'path';
 import * as vscode from 'vscode';
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
-	statusBarItem.text = '$(git-branch)';
-	statusBarItem.tooltip = 'click to select a branch';
-	statusBarItem.command = 'extension.openBranchSelector';
-	statusBarItem.show();
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "my-favorite-branch" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.openBranchSelector', async () => {
-		// The code you place here will be executed every time your command is executed
-		let repository;
-		if (vscode.window.activeTextEditor?.document.uri) {
-
-			repository = vscode.workspace.getWorkspaceFolder(vscode.window?.activeTextEditor?.document.uri);
-		}
-		if (repository) {
-			const git = vscode.extensions.getExtension('vscode.git')?.exports;
-			const branches = await git?.getRepository(repository.uri)?.getBranches();
-			if (branches) {
-				const branchNames = branches.map((branch: any) => branch.name);
-				const selectedBranch = await vscode.window.showQuickPick(branchNames);
-				if (selectedBranch) {
-					// TODO: Do something with the selected branch
-					console.log(`Selected branch: ${selectedBranch}`);
-				}
-			} else {
-				vscode.window.showErrorMessage('Failed to get the branch list');
-			}
-		} else {
-			vscode.window.showErrorMessage('This command only works in a workspace folder');
-		}
+	let openDrawer = vscode.commands.registerCommand('extension.openDrawer', () => {
 
 	});
+	context.subscriptions.push(openDrawer);
+	context.subscriptions.push(createStatusBarItem());
 
-	context.subscriptions.push(disposable);
+	let checkoutFavoriteBranch = vscode.commands.registerCommand('extension.openBranchSelector', () => {
+		const branch = vscode.window.showQuickPick(getFavoriteBranches(), { canPickMany: false, placeHolder: 'My favorite branches' });
+	});
+
+
+	context.subscriptions.push(checkoutFavoriteBranch);
 }
 
-// function getWebviewContent(repo: vscode.WorkspaceFolder): string {
-// 	return `        <!DOCTYPE html>
-//         <html>
-//         <head>
-//             <meta charset="UTF-8">
-//             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//             <title>Branch Selector</title>
-//         </head>
-//         <body>
-//             <h1>Branch Selector</h1>
-//             <ul>
-//                 ${getBranches(repo).map(branch => `<li>${branch}</li>`).join('')}
-//             </ul>
-//         </body>
-//         </html>`;
-// }
+function createStatusBarItem() {
+	const iconPath = {
+		light: path.join(__filename, '..', 'resources', 'light', 'my-icon.png'),
+		dark: path.join(__filename, '..', 'resources', 'dark', 'my-icon.png')
+	};
+	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	statusBarItem.text = '$(my-icon)';
+	statusBarItem.tooltip = 'Click to open drawer';
+	statusBarItem.command = 'myExtension.openDrawer';
+	statusBarItem.color = '#fff';
+	statusBarItem.show();
+	return statusBarItem;
+}
+function getFavoriteBranches(): { id: string, label: string, description: string, detail: string; }[] => {
+	return [
+		{ id: 'test1', label: 'Item 1', description: 'Description for Item 1', detail: 'Additional details for Item 1' },
+		{ id: 'test2', label: 'Item 2', description: 'Description for Item 2', detail: 'Additional details for Item 2' },
+		{ id: 'test3', label: 'Item 3', description: 'Description for Item 3', detail: 'Additional details for Item 3' },
+	];
+};
 
-// function getBranches(repo: vscode.WorkspaceFolder): string[] {
-// 	return ['master', 'dev', 'feature/z'];
-// }
+
+function getCurrentBranch(): string {
+	const gitExtension = vscode.extensions.getExtension('vscode.git')?.exports;
+	const git = gitExtension.getAPI(1);
+	console.log('git', git.arg1.git);
+	const repo = git.repositories[0];
+	const currentBranch = repo.state.head ? repo.state.head.name : 'Detached head state';
+	return currentBranch;
+
+
+};
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
